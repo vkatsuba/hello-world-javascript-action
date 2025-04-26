0fe0f59a9e6414445b2ca6353ac5723a22a79d4e
vk <v.katsuba.dev@gmail.com>
2025-04-26 12:41:08 +0300

    Test branch

--- dist/index.js
+++ dist/index.js
@@ -31832,7 +31832,7 @@ const github = __nccwpck_require__(5100);
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  console.log(`Hello ${nameToGreet}!!!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
